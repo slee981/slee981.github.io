ea3649337772dd72b3d3758ad53e5bc9eeb9fd8d
Stephen Lee <smlee.981@gmail.com>
2019-03-13 09:33:44 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+
+  var r = document.getElementsByClassName("right-nav");
+  r.className += " responsive";
+}
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("menu").style.padding = "5px 30px";
+  } else {
+    document.getElementById("menu").style.padding = "10px 30px";
+  }
+}
