a86579f6b2e920b88182e08d99da3dce188ad228
Stephen Lee <smlee.981@gmail.com>
2019-06-20 16:30:37 -0500

    pad top of navbar on mobile, make normal size on desktop

--- js/main.js
+++ js/main.js
@@ -19,12 +19,31 @@ function myFunction() {
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
+// media match 
+var x = window.matchMedia("(max-width: 1200px)")
+
 function scrollFunction() {
+
+  // if you are scrolling
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("menu").style.padding = "15px 10px";
     document.getElementById("menu").style.borderBottom = "2px solid rgb(65, 65, 65)";
+    // mobile
+    if (x.matches) {
+      document.getElementById("menu").style.padding = "60px 10px 10px";
+    // desktop
+    } else {
+      document.getElementById("menu").style.padding = "20px 10px 5px";
+    }
+
+  // if you are not scrolling
   } else {
-    document.getElementById("menu").style.padding = "20px 10px";
     document.getElementById("menu").style.borderBottom = "0.5px solid rgb(65, 65, 65)";
+    // mobile
+    if (x.matches) {
+      document.getElementById("menu").style.padding = "60px 10px 20px";
+    // desktop
+    } else {
+      document.getElementById("menu").style.padding = "20px 10px";
+    }
   }
 }
