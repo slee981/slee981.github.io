daef47d2b143772721c6c7e94610a9e45b5d9b15
Stephen Lee <smlee.981@gmail.com>
2019-12-19 18:13:05 -0600

    change function name

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleTopNav() {
   var x = document.getElementById("menu");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -15,7 +15,7 @@ function myFunction() {
 }
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 // media match 
 var x = window.matchMedia("(max-width: 1200px)")
@@ -28,18 +28,18 @@ function scrollFunction() {
     // mobile
     if (x.matches) {
       document.getElementById("menu").style.padding = "60px 10px 10px";
-    // desktop
+      // desktop
     } else {
       document.getElementById("menu").style.padding = "20px 10px 5px";
     }
 
-  // if you are not scrolling
+    // if you are not scrolling
   } else {
     document.getElementById("menu").style.borderBottom = "0.5px solid rgb(65, 65, 65)";
     // mobile
     if (x.matches) {
       document.getElementById("menu").style.padding = "60px 10px 20px";
-    // desktop
+      // desktop
     } else {
       document.getElementById("menu").style.padding = "20px 10px";
     }
